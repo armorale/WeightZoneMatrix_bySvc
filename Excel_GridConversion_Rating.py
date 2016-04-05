76c7fb1b5fc0be3517a533949854c3b1e436287a
armorale <armorale@gmail.com>
2016-04-04 20:37:09 -0700

    Update Excel_GridConversion_Rating.py

--- Excel_GridConversion_Rating.py
+++ Excel_GridConversion_Rating.py
@@ -13,6 +13,7 @@ delimitedData = list(readdataFile)
 opendetailFile.close()
 
 #find out what indexes weight and zone are in and set those equal to variables. Regex?
+delimitedData[0] = [x.lower() for x in delimitedData[0]] #makes the whole header lowercase -- not sure why, list comprehension
 
 zoneWord = []
 
@@ -37,32 +38,22 @@ if len(weightWord)==0:
 
 #find out the index (columns) that the zone and weight are in 
 
-delimitedData[0] = [x.lower() for x in delimitedData[0]] #makes the whole header lowercase -- not sure why, list comprehension
+#delimitedData[0] = [x.lower() for x in delimitedData[0]] #makes the whole header lowercase -- not sure why, list comprehension
 
 weightIndex = delimitedData[0].index(str(weightWord[0]))
 zoneIndex = delimitedData[0].index(str(zoneWord[0]))
-
-#create my excel grid with weight range up to 150. Each weight represents its own list
-
-weightNumbers = list(range(0,151))
-weightNumbers.insert(0,'Weight(lbs)') #create a list from weight up to 150lbs
-
-numberGrid = [[] for _ in range(len(weightNumbers))] #creating a format of lists of lists must be a more efficient way to do this
-
-for i in range(len(weightNumbers)): #the for loop allows me to drop in my weights in the lists of lists. Essentially my left hand column
-	numberGrid[i].append(weightNumbers[i])
 
 #find the unique zones in the sheet
 
 uniqueZones = set()
 
-for i in range(len(delimitedData)):
-	uniqueZones.add(delimitedData[i][19])
+for i in range(1,len(delimitedData)):
+	uniqueZones.add(delimitedData[i][zoneIndex])
 
 uniqueZonesList = list(uniqueZones)
-#build a function to search for integer zones, sort 1-8 and then append alphavetzones and sort a-z
+#build a function to search for integer zones, sort 1-8 and then append alphabetzones and sort a-z
 
-def createZoneHeader(orderZones): #all of this is stuck in the local scope now
+def createZoneHeader(orderZones): #function to return a header of the unique zones in the data set
 	numericZones = []
 	numericZonesSorted = []
 	alphabeticZones = []
@@ -77,7 +68,48 @@ def createZoneHeader(orderZones): #all of this is stuck in the local scope now
 	alphabeticZonesSorted = sorted(alphabeticZones)
 	combinedHeader = []
 	combinedHeader = numericZonesSorted + alphabeticZonesSorted
-	print(combinedHeader)
+	return combinedHeader
 
-zoneHeader = createZoneHeader(uniqueZonesList) #this returns nothing the 2nd time I enter it
+zoneHeader = createZoneHeader(uniqueZonesList) 
 
+#find the uniques service codes for the data set
+
+def createdServiceGroup(dataSet):
+	svcWord=[]
+	uniqueServices=set()
+	for i in range(len(dataSet[0])):
+		uniqueColumnName = dataSet[0][i]
+		print(uniqueColumnName)
+		if 'svc' or 'service' in str(uniqueColumnName):
+			print(uniqueColumnName)
+#			svcWord.append(dataSet[0][i])
+		else:
+			print('There is no service column.')
+#	if len(svcWord)==0:
+#		print('There is no service column.')
+	svcIndex = dataSet[0].index(str(svcWord[0]))
+	for i in range(1,len(dataSet)):
+		uniqueServices.add(dataSet[i][svcIndex])
+	uniqueServicesList = sorted(list(uniqueServices))
+	return uniqueServicesList
+
+allServices = createdServiceGroup(delimitedData)
+
+
+
+
+zoneHeader.insert(0,'Weight(lbs)')
+
+#create my excel grid with weight range up to 150. Each weight represents its own list
+
+weightNumbers = list(range(0,151))
+#weightNumbers.insert(0,zoneHeader) #need to create a list within the list. Right now it is a string file
+
+
+numberGrid = [[] for _ in range(len(weightNumbers))] #creating a format of lists of lists must be a more efficient way to do this
+
+for i in range(len(weightNumbers)): #the for loop allows me to drop in my weights in the lists of lists. Essentially my left hand column
+	numberGrid[i].append(weightNumbers[i])
+
+numberGrid.insert(0,[])
+numberGrid[0].extend(zoneHeader)
